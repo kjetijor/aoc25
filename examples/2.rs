87c592e89bffd7b6862c00db3a2d890f2c2da782
Kjetil Joergensen <kjetijor@gmail.com>
2025-12-04 11:50:54 -0800

    day 2 finished

--- examples/2.rs
+++ examples/2.rs
@@ -1,4 +1,4 @@
-use aoc_25::invalidids::{iter_ranges, naive_invalid_id, naive_invalid_ids};
+use aoc_25::invalidids::{iter_ranges, naive_invalid_id, naive_invalid_id_pt2, naive_invalid_ids};
 use std::fs::File;
 
 fn main() {
@@ -8,10 +8,12 @@ fn main() {
         std::process::exit(1);
     }
     let mut res = 0u64;
+    let mut res_pt2 = 0u64;
     let file = File::open(&args[1]).expect("Failed to open input file");
     for r in iter_ranges(file) {
         let r = r.expect("Failed to parse range");
         res += naive_invalid_ids(&r, naive_invalid_id).into_iter().sum::<u64>();
+        res_pt2 += naive_invalid_ids(&r, naive_invalid_id_pt2).into_iter().sum::<u64>();
     }
-    println!("Sum of invalid IDs: {}", res);
+    println!("Sum of invalid IDs: {}, Sum of invalid IDs using second method {}", res, res_pt2);
 }
